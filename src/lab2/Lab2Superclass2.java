998df93d6d4d00cfb7626d1e5a783ec2f04f2445
mreiff <mreiff@L15201039986.wctc.msft>
2014-09-04 13:16:39 -0500

    extended lab2superclass to lab2superclass2.

---
+++ src/lab2/Lab2Superclass2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab2;
+
+/**
+ *
+ * @author mreiff
+ */
+public interface Lab2Superclass2 extends Lab2Superclass{
+    public abstract void prerequisites();
+    // for saving purposes
+}
