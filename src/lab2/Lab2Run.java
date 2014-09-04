d9ddf9c6175b555f8638a93f61cfc9eb9a99bfcf
mreiff <mreiff@L15201039986.wctc.msft>
2014-09-04 14:15:53 -0500

    made changes to the lab2run class

--- src/lab2/Lab2Run.java
+++ src/lab2/Lab2Run.java
@@ -12,6 +12,26 @@ package lab2;
  */
 public class Lab2Run {
     public static void main(String[] args) {
+        Lab2Superclass introToProg = new IntroToProgrammingCourse();
+        SecondLabSuperclass2 introToJava = new IntroJavaCourse();
+        SecondLabSuperclass2 advancedJava = new AdvancedJavaCourse();
         
+        System.out.println("Intro To Programming");
+        System.out.println("Prerequisites = None");
+        introToProg.credits(); System.out.println(" credits");
+        introToProg.courseNumber(); System.out.println(" is the course number.");
+        introToProg.courseName(); System.out.println(" is the course name.");
+        
+        System.out.println("Intro To Java");
+        introToJava.prerequisites(); System.out.println(" are the prerequisites");
+        introToJava.credits(); System.out.println(" credits");
+        introToJava.courseNumber(); System.out.println(" is the course number.");
+        introToJava.courseName(); System.out.println(" is the course name.");
+        
+        System.out.println("Advanced Java");
+        advancedJava.prerequisites(); System.out.println(" are the prerequisites");
+        advancedJava.credits(); System.out.println(" credits");
+        advancedJava.courseNumber(); System.out.println(" is the course number.");
+        advancedJava.courseName(); System.out.println(" is the course name.");
     }
 }
