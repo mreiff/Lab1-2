c561378dc9d075f274326a5d40d726dd5b455b07
mreiff <mreiff@L15201039986.wctc.msft>
2014-09-04 11:57:48 -0500

    This is my initial add to github. There have been some changes done already to the program.

---
+++ src/lab1/Lab1Superclass.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab1;
+
+/**
+ *
+ * @author mreiff
+ */
+public abstract class Lab1Superclass {
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+    
+     public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+}
